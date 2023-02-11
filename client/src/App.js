0cfedec8afa5a9055cde4490272bca2bc67c2f5f
Gokul190902 <gokul190902@gmail.com>
2023-02-11 12:06:37 +0530

    Cards

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.scss";
 import Categories from "./Categories";
 import Home from "./Home";
+import Topdest from "./Topdest";
 
 const App = () => {
   return (
     <div id="app">
       <Home />
       <Categories />
+      <Topdest />
     </div>
   );
 };
