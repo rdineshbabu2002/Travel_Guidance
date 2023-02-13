1f12ea8abdee96807a64107219d9adb10cf6ef6e
DINESHBABU R <90496628+DINESHBABU2002@users.noreply.github.com>
2023-02-13 18:34:25 +0530

    featuresadded

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -3,6 +3,7 @@ import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Locations from "./Pages/Locations/Locations";
 import Detailed from "./Pages/Detailed/Detailed";
+import Payment from "./Components/Payment/Payment";
 const AllRoutes = () => {
   return (
     <Routes>
@@ -11,6 +12,7 @@ const AllRoutes = () => {
       <Route path="/locations/:name" element={<Locations />} />
 
       <Route path="/Detailed/:name" element={<Detailed />} />
+      <Route path="/payment" element={<Payment />} />
     </Routes>
   );
 };
