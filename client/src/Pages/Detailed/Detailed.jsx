cfb6e2908e9aa3a90aed5bcad04eb973e0bb4dea
DINESHBABU R <90496628+DINESHBABU2002@users.noreply.github.com>
2023-02-14 21:27:51 +0530

    dataadded

--- client/src/Pages/Detailed/Detailed.jsx
+++ client/src/Pages/Detailed/Detailed.jsx
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./Detailed.scss";
 import Navbar from "../../Components/Navbar/Navbar";
-import img from "../../assests/Lotus.jpg";
 import CustomMap from "../../Components/CustomMap/CustomMap";
 import Hotels from "../../Components/Hotelscard/Hotels";
 const Detailed = () => {
@@ -19,17 +18,19 @@ const Detailed = () => {
   }, []);
 
   if (place == null) return <p>Loading....</p>;
+  console.log(place);
 
   return (
     <div id="detailedinfo">
       <Navbar />
       <div className="detail">
         <h1 className="titlehead">{place["Place Name"]}</h1>
-        <img className="imgdetail" src={img} alt="img" />
+        <img className="imgdetail" src={place["Image"]} alt="img" />
         <p className="description">{place.Description}</p>
         <h1 className="titlehead"> G-Map Location</h1>
         <CustomMap lat={place?.Latitude} lng={place?.Longitude} />
         <h1 className="titlehead">Hotels Nearby </h1>
+
         <div className="hotellist">
           {place["Hotels near the Place"].map((hotel, idx) => {
             return (
--- client/src/Pages/Locations/Locations.jsx
+++ client/src/Pages/Locations/Locations.jsx
@@ -8,7 +8,7 @@ const Locations = () => {
   const [places, setPlaces] = useState([]);
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get(`/locations/${name}`)
+      const { data } = await axios.get(`/locations/${name}`);
       setPlaces([...data]);
     };
     getData();
@@ -17,37 +17,34 @@ const Locations = () => {
   return (
     <div>
       <Navbar />
-      {places.map((place, index) => {
-        return (
-          <div>
-            <h1 className="head">
-              Top Destinations in{" "}
-              {name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()}
-            </h1>
-            <p className="para">
-              {" "}
-              Much places suits your mood.Explore somewhere interesting
-              <br /> and enjoy the vibes!
-            </p>
-            <div className="card-container-1">
-              <div className="cards">
-                {places.map((data, index) => {
-                  return (
-                    <Card
-                      key={index}
-                      name={place["Place Name"]}
-                      desc={place.Description}
-                      loc={place.Location}
-                      image_url={place.image_url}
-                      id={data._id}
-                    />
-                  );
-                })}
-              </div>
-            </div>
+      <div>
+        <h1 className="head">
+          Top Destinations in{" "}
+          {name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()}
+        </h1>
+        <p className="para">
+          {" "}
+          Much places suits your mood.Explore somewhere interesting
+          <br /> and enjoy the vibes!
+        </p>
+        <div className="card-container-1">
+          <div className="cards">
+            {places.map((place, index) => {
+              return (
+                <Card
+                  key={index}
+                  name={place["Place Name"]}
+                  desc={place.Description}
+                  loc={place.Location}
+                  image_url={place.Image}
+                  id={place._id}
+                />
+              );
+            })}
           </div>
-        );
-      })}
+        </div>
+      </div>
+      );
     </div>
   );
 };
