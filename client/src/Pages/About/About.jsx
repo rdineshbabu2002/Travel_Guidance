01fe1d03263fbc3be4434d60ba018a915cee21ab
Gokul190902 <gokul190902@gmail.com>
2023-02-18 07:20:43 +0530

    AboutUsPageadded

--- client/src/Pages/About/About.jsx
+++ client/src/Pages/About/About.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import "./About.scss";
+import Aboutcont from "./Aboutcont";
+import Footer from "../../Components/Footer/Footer";
 
 const About = () => {
   return (
     <div id="About">
       <Navbar />
+      <Aboutcont />
+      <Footer />
     </div>
   );
 };
